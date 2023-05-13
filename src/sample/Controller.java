af17ac46edc0768f6b6a69c2fb4f63c72b680f66
dan_stadt <dstadt14@gmail.com>
2023-05-12 21:35:54 -0600

    first save

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Text usernameText;
+    public TextField usernameField;
+    public Text passwordText;
+    public PasswordField passwordField;
+    public Text locationText;
+    public TextField locationField;
+    public Text loginText;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Locale locale = Locale.getDefault();
+        if (locale.getLanguage().equals("en")){
+            System.out.println("English");
+        }
+        if (locale.getLanguage().equals("fr")){
+            System.out.println("french");
+        }
+    }
+
+    public void enterClick(ActionEvent actionEvent) {
+    }
 }
