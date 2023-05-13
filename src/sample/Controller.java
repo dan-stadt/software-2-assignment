c1a15c79f2406e51720b851103738fa34320c3e3
dan_stadt <dstadt14@gmail.com>
2023-05-12 22:45:42 -0600

    first save

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,11 +2,13 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -22,14 +24,30 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Locale locale = Locale.getDefault();
+        ZoneId zoneId = ZoneId.systemDefault();
+        locationField.setText(zoneId.toString());
         if (locale.getLanguage().equals("en")){
             System.out.println("English");
         }
         if (locale.getLanguage().equals("fr")){
-            System.out.println("french");
+            usernameText.setText("Nom d'utilisateur");
+            passwordText.setText("Mot de passe");
+            loginText.setText("Connexion");
+            locationText.setText("Emplacement");
         }
     }
 
-    public void enterClick(ActionEvent actionEvent) {
+    public void enterClick(ActionEvent actionEvent) {loginCheck();}
+    public void enterPassword(ActionEvent actionEvent) {loginCheck();}
+
+    public void enterUsername(ActionEvent actionEvent) {loginCheck();}
+    public void loginCheck(){
+        if(true){
+
+
+            Alert error = new Alert(Alert.AlertType.ERROR,"Username & password not found.");
+            error.showAndWait();
+        }
+
     }
 }
