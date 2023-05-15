53c3e460d7df9260f332ae6ec546d7d428eff6f2
dan_stadt <dstadt14@gmail.com>
2023-05-15 14:15:12 -0600

    second save

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,4 @@
-package sample;
+package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
@@ -12,7 +12,7 @@ import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class LoginController implements Initializable {
     public Text usernameText;
     public TextField usernameField;
     public Text passwordText;
@@ -20,6 +20,7 @@ public class Controller implements Initializable {
     public Text locationText;
     public TextField locationField;
     public Text loginText;
+    private String username;    //TODO: Pull username forward for update field
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -39,15 +40,13 @@ public class Controller implements Initializable {
 
     public void enterClick(ActionEvent actionEvent) {loginCheck();}
     public void enterPassword(ActionEvent actionEvent) {loginCheck();}
-
     public void enterUsername(ActionEvent actionEvent) {loginCheck();}
     public void loginCheck(){
+        boolean loginResult;
         if(true){
-
-
             Alert error = new Alert(Alert.AlertType.ERROR,"Username & password not found.");
             error.showAndWait();
         }
-
+        //TODO: Continually update login_activity.txt with date, time, and if successful
     }
 }
---
+++ src/helper/CustomerQuery.java
@@ -0,0 +1,32 @@
+package helper;
+
+import sample.JDBC;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CustomerQuery {
+    public static enum customerFields{
+        Customer_ID, Customer_Name, Address, Postal_Code, Phone, Create_Date, Created_By, Last_Update, Last_Updated_By, Division_ID;
+    }
+    public static int insertCustomer (String name, String address, String postalCode, String phone, int division) throws SQLException {
+        customerFields.values();
+        String sql = "INSERT INTO customers (" + customerFields.values() + ") VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?"; //TODO: Confirm works
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        int id = 1;         //TODO: Update to select unused ID
+        String date = "";    //TODO: Update to calculate today's today
+        String user = "";    //TODO: Update to match current user, taken from login
+        ps.setInt(1, id);
+        ps.setString(2, name);
+        ps.setString(3, address);
+        ps.setString(4, postalCode);
+        ps.setString(5, phone);
+        ps.setString(6, date);
+        ps.setString(7, user);
+        ps.setString(8, date);
+        ps.setString(9, user);
+        ps.setInt(10, division);
+        int rowsAffected = ps.executeUpdate();
+        return rowsAffected;
+    }
+}
